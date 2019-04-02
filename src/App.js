c75e0c9b95dcfd197eeb4ccca9e9f6b667e74b9c
jeffau13 <jeffau13@gmail.com>
2019-04-02 15:26:17 -0400

    used json placeholder to simulate a backend for storing and updating todo items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import uuid from 'uuid';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
   state = {
-    todos: [
-      { id: uuid.v4(), title: 'take out trash', completed: true },
-      { id: uuid.v4(), title: 'watch movie with driends', completed: false },
-      { id: uuid.v4(), title: 'make react app', completed: false }
-    ]
+    todos: []
   };
+  componentDidMount() {
+    Axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(
+      res => this.setState({ todos: res.data })
+    );
+  }
 
   markComplete = id => {
     this.setState({
@@ -28,16 +29,22 @@ class App extends Component {
   };
 
   delTodo = id => {
-    this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    });
+    Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
   };
 
   addTodo = title => {
-    const newTodo = { id: uuid.v4(), title: title, completed: false };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
+    Axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false
+    }).then(res =>
+      this.setState({
+        todos: [...this.state.todos, res.data]
+      })
+    );
   };
 
   render() {
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 export default class AddTodo extends Component {
   state = {
     title: ''
@@ -39,3 +39,9 @@ export default class AddTodo extends Component {
     );
   }
 }
+
+AddTodo.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
